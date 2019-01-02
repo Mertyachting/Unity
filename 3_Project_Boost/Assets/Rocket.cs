6aef178499b95a822ca54b51849daf2063a0d021
Mert Sevindik <mryachting@outlook.de>
2019-01-02 20:55:53 +0100

    Added Sound effects when you press spacekey soundeffect from : https://freesound.org/people/LimitSnap_Creations/sounds/318688/

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.Experimental.PlayerLoop;
 
 public class Rocket : MonoBehaviour {
 
 	Rigidbody rigidbody;
+	AudioSource audiosource;
 	
 	// Use this for initialization
 	void Start ()
 	{
 	rigidbody = GetComponent<Rigidbody>();
+	audiosource = GetComponent<AudioSource>();
+	//audiosource.Play(0);
+	//audiosource.Pause();
+		
 	}
 	
 	// Update is called once per frame
@@ -21,9 +27,21 @@ public class Rocket : MonoBehaviour {
 
 	private void ProcessInput()
 	{
+		audiosource.Pause();
+		
 		if (Input.GetKey(KeyCode.Space))
 		{
 			rigidbody.AddRelativeForce(Vector3.up);
+
+			if (!audiosource.isPlaying)
+			{
+				audiosource.Play(0);
+			}
+			else
+			{
+				audiosource.Stop();
+			}
+			//audiosource.UnPause();
 		}
 
 		if (Input.GetKey(KeyCode.A))
@@ -35,5 +53,6 @@ public class Rocket : MonoBehaviour {
 		{
 			transform.Rotate(-Vector3.forward);
 		}
+		
 	}
 }
